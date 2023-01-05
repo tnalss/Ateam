191c20a9cbb7884a0c80db4d9fb35aee6b5a1a35
jooyoung <jooyoung.cairo@gmail.com>
2023-01-05 17:50:49 +0900

    Update attend_pro.sql

--- attend_pro.sql
+++ attend_pro.sql
@@ -1,25 +1,39 @@
-create or replace procedure pro_attend( emp number ) is 
-    cnt  number;
-begin
-    select count(emp_no) into cnt from tbl_attend  
-    where to_char(attend_date, 'yyyy/mm/dd hh:mi:ss') = to_char(sysdate, 'yyyy/mm/dd hh:mi:ss') and emp_no = emp ;
-    
-    if( cnt=0 ) then
-        
-    else 
-        update tbl_attend set attend_off = sysdate, att_code ='W1',att_state='퇴근'
-        where emp_no = emp and to_char(attend_date, 'yyyy/mm/dd hh:mi:ss') = to_char(sysdate, 'yyyy/mm/dd hh:mi:ss');
-        
-    end if;
-end;
-/
-
---delete from tbl_attend where emp_no= 1 and to_char(attend_date, 'yymmdd')='230105';
-commit;
---, to_char(attend_date, 'yyyy/mm/dd hh:mi:ss')aa
-
+--create or replace procedure pro_attend( emp number ) is 
+--    cnt  number;
+--begin
+--    select count(emp_no) into cnt from tbl_attend  
+--    where to_char(attend_date, 'yyyy/mm/dd hh:mi:ss') = to_char(sysdate, 'yyyy/mm/dd hh:mi:ss') and emp_no = emp ;
+--    
+--    if( cnt=0 ) then
+--        
+--    else 
+--        update tbl_attend set attend_off = sysdate, att_code ='W1',att_state='퇴근'
+--        where emp_no = emp and to_char(attend_date, 'yyyy/mm/dd hh:mi:ss') = to_char(sysdate, 'yyyy/mm/dd hh:mi:ss');
+--        
+--    end if;
+--end;
 --출퇴근처리
-exec pro_attend(4);
+--exec pro_attend(4);
+
+--/
+
+
+--출근 
+insert into tbl_attend (attend_date,emp_no,attend_on,attend_off,att_code,att_state)
+values(sysdate,6,to_char(sysdate,'yyyy/mm/dd hh24:mi:ss'),
+        to_char(sysdate,'yyyy/mm/dd hh24:mi:ss'),'W0','출근')  ;
+
+
+--퇴근
+UPDATE tbl_attend
+SET attend_off = to_char(sysdate,'yyyy/mm/dd hh24:mi:ss') , att_code='W1', att_state='퇴근'
+WHERE emp_no=6; 
+
+
+
+commit;
+
+
 
 
 --전체목록조회
@@ -32,13 +46,13 @@ on a.emp_no = e.emp_no;
 select a.emp_no, e.emp_name ,TO_CHAR(a.ATTEND_ON,'yyyy/mm/dd hh24:mi:ss') ATTEND_ON, TO_CHAR(a.ATTEND_off,'yyyy/mm/dd hh24:mi:ss') ATTEND_off,att_state
 from tbl_attend a inner join tbl_emp e
 on a.emp_no = e.emp_no
-and a.emp_no = 1;
+and a.emp_no = 6;
 
 --사원 한명의 출퇴근 조회 (전체날짜)
 select a.emp_no, e.emp_name , a.attend_date, a.att_state
 from tbl_attend a inner join tbl_emp e
 on a.emp_no = e.emp_no
-and a.emp_no = 1;
+and a.emp_no = 6;
 
 
 
@@ -48,24 +62,9 @@ from tbl_attend a left join tbl_emp e
 on a.emp_no = e.emp_no
 order by emp_no , attend_on;
 
-SELECT *
- FROM tbl_attend 
- WHERE  att_state = '퇴근' ;
---
-
---update 조건 . ?
-create or replace procedure pro_attend_state  is
-begin 
-    update tbl_attend set att_state = '정상' WHERE  att_state = '퇴근' or att_state ='출근' ; 
-end;
 
 
 
---exec pro attend_state;
---rollback;
-
-
---grant create any job to a_team;
 
 
 
