0b3ff9e0155ab446094e1704748546ebc40bc7d1
ParkChanJae <ekzm2302@naver.com>
2023-01-21 04:08:14 +0900

    공지사항 페이징
    아직 ...

--- jkcompany/src/main/java/co/kr/jkcompany/NoticeController.java
+++ jkcompany/src/main/java/co/kr/jkcompany/NoticeController.java
@@ -12,7 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import common.CommonService;
+import employee.EmployeePageVO;
 import employee.EmployeeVO;
+import notice.NoticePageVO;
 import notice.NoticeVO;
 
 @Controller
@@ -23,18 +25,38 @@ public class NoticeController {
 	private CommonService common;
 	
 		//조회하고 출력하는 예제
-		@RequestMapping(value= "/list.no" , produces="text/html;charset=utf-8")
-		public String notice_list(HttpSession session, Model model) {
-			//각 컨트롤러 입장 메소드는 category에 속성을 넣어주세요!
-			session.setAttribute("notice", "no" );
-			List<NoticeVO> list = sql.selectList("no.list");
-			//조회해온 값을 모델로 list라는 곳에 담았습니다.
-			model.addAttribute("list", list);			
-			//리턴을 통해 employee 폴더에 list.jsp 를 찾아갑니다.
+//		@RequestMapping(value= "/list.no" , produces="text/html;charset=utf-8")
+//		public String notice_list(HttpSession session, Model model) {
+//			//각 컨트롤러 입장 메소드는 category에 속성을 넣어주세요!
+//			session.setAttribute("notice", "no" );
+//			List<NoticeVO> list = sql.selectList("no.list");
+//			//조회해온 값을 모델로 list라는 곳에 담았습니다.
+//			model.addAttribute("list", list);			
+//			//리턴을 통해 employee 폴더에 list.jsp 를 찾아갑니다.
+//			return "notice/list";
+//		}
+	
+	// 조회하고 출력하는 예제
+		@RequestMapping(value = "/list.no", produces = "text/html;charset=utf-8")
+		public String notice_list(HttpSession session, Model model,NoticePageVO page) {
+
+			// 각 컨트롤러 입장 메소드는 category에 속성을 넣어주세요!
+			session.setAttribute("notice", "no");
+			model.addAttribute("page", notice_list(page) );
+			
 			return "notice/list";
 		}
 		
-		// 사번으로 사원 정보조회
+		
+		// 페이지 처리
+		public NoticePageVO notice_list(NoticePageVO page) {
+			page.setTotalList( sql.selectOne("no.total", page) ); 
+			page.setList( sql.selectList("no.plist", page) );
+			return page;
+		}
+		
+		
+		// 제목으로 상세내용
 		@RequestMapping(value= "/info.no" , produces="text/html;charset=utf-8")
 		public String emp_info(String id, Model model) {
 			NoticeVO vo = sql.selectOne("no.info",id);
